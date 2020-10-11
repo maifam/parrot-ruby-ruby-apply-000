a15c6ce0307b3347d83e2ef753e5ec7db0f8caa0
Mai Pham <maitphm@gmail.com>
2020-10-11 00:02:52 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase)
-  
-  return 
-end 
 
-parrot("squawk")
+def parrot(string="Squawk!")
+  puts "#{string}"
+  "#{string}"
+end
